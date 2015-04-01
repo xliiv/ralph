0e1637c1723ff60fb4a6402b06aa6e482019909f
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2015-04-01 15:13:04 +0200

    Updated changelog & bumped bob-ajax-selects version.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     package_dir={'': 'src'},
     zip_safe=False,  # because templates are loaded from file path
     install_requires=[
-        'bob-ajax-selects==1.4.1',
+        'bob-ajax-selects==1.5',
         'djangorestframework==2.4.3',
         'django-bob==1.10.0',
         'django-powerdns-dnssec==0.9.3',
