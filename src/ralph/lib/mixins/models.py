2b5bc848400d606ac6000066e03b74f4fac375a6
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2015-07-31 07:44:03 +0200

    Improved lookup-widget.
    Added:
    - `+` icon for fast adding new objects
    - `pencil` icon for fast editing related objects
    
    Fixed getting related fields in `search form` & `autocomplete widget`.

--- src/ralph/lib/mixins/models.py
+++ src/ralph/lib/mixins/models.py
@@ -1,8 +1,45 @@
 # -*- coding: utf-8 -*-
+from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+
+
+def get_admin_url(obj, action):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    return reverse(
+        "admin:{}_{}_{}".format(
+            content_type.app_label, content_type.model, action
+        ),
+        args=(obj.id,)
+    )
+
+
+def get_field_by_relation_path(model, field_path):
+    """
+    Returns field for `model` referenced by `field_path`.
+
+    E.g. calling:
+        get_field_by_relation_path(BackOfficeAsset, 'model__manufacturer__name')
+    returns:
+        <django.db.models.fields.CharField: name>
+    This is achieved by dynamically executing such code:
+        self.model.\
+        _meta.get_field('model').related_model.\
+        _meta.get_field('manufacturer').related_model.\
+        _meta.get_field('name')
+    """
+    def get_related_model(model, field_name):
+        related_model = model._meta.get_field(field_name).related_model
+        return related_model
+
+    hops = field_path.split('__')
+    relation_hops, dst_field = hops[:-1], hops[-1]
+    for field_name in relation_hops:
+        model = get_related_model(model, field_name)
+    found_field = model._meta.get_field(dst_field)
+    return found_field
 
 
 class NamedMixin(models.Model):
